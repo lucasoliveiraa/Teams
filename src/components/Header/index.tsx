19006afc5345bf5107e223896a77641a6f23b17c
lucasoliveiraa <lucasjackcosta18@gmail.com>
2022-10-11 23:14:24 -0300

    feat: css-in-js

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { Container, Logo, BackButton, BackIcon } from "./styles";
+
+import logoImg from "@assets/logo.png";
+
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
+  return (
+    <Container>
+      <BackButton>
+        <BackIcon />
+      </BackButton>
+      <Logo source={logoImg} />
+    </Container>
+  );
+}
